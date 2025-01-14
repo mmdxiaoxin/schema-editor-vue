559e75e8afff5237c7256df3ec7c793c25fc26c0
小新 <782446723@qq.com>
2025-01-14 19:45:14 +0800

    feat: 编辑器行组件

--- src/App.vue
+++ src/App.vue
@@ -2,35 +2,9 @@
   <div>
     <h1>JSON Editor</h1>
     <SchemaItem />
-    <pre>{{ jsonData }}</pre>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, reactive } from 'vue'
-import JSONEditor from './components/JSONEditor.vue'
-import SchemaBasic from './components/basic/SchemaBasic.vue'
+<script lang="ts" setup>
 import SchemaItem from './components/SchemaItem.vue'
-
-export default defineComponent({
-  components: { JSONEditor, SchemaBasic, SchemaItem },
-  setup() {
-    const jsonData = reactive([
-      {
-        key: 'root',
-        value: '',
-        children: [
-          { key: 'child1', value: 'value1' },
-          { key: 'child2', value: 'value2' },
-        ],
-      },
-    ])
-
-    const updateJson = (updatedData: any) => {
-      console.log('Updated JSON Data:', updatedData)
-    }
-
-    return { jsonData, updateJson }
-  },
-})
 </script>
---
+++ src/components/SchemaItem.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <SchemaBasic />
+  </div>
+</template>
+
+<script setup lang="ts">
+import SchemaBasic from './basic/SchemaBasic.vue'
+</script>
+
+<style scoped></style>
