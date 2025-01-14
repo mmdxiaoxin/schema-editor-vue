f2a1738d474269122dd5cb51b0d7b81303d60efd
小新 <782446723@qq.com>
2025-01-14 19:44:30 +0800

    chore: 引入elementPlus

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
+import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+import 'element-plus/dist/index.css'
 import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
+app.use(ElementPlus)
+app.mount('#app')
